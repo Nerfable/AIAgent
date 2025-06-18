c2824965a49dd0488f411a8a8f70bab59b7dd11e
Nerfable <mario.veit@hotmail.de>
2025-06-19 01:28:36 +0200

    CH1 L4 done.

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
+if len(sys.argv) != 2:
+    print("Script needs a  Prompt to run.")
+    sys.exit(1)
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key="AIzaSyDT_w99-fVgYuKBg7KUwwQsu8SNI6Bz7og")
-
+prompt = sys.argv[1]
 result = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model="gemini-2.0-flash-001", contents=prompt
     )
 print(result.text)    
 print(f"Prompt tokens: {result.usage_metadata.prompt_token_count}")
